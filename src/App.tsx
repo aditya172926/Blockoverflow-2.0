26fc617c9bfe1bc6d632d84048e54e2248b5b3f7
Aditya <aditya26sg@gmail.com>
2024-06-25 23:32:48 +0530

    tailwind css integration fixed

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,19 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 
+
 function App() {
 
   return (
     <div className="container">
-      
+      <div className="flex flex-row">
+        <div className="basis-1/4">
+          <p>File column</p>
+        </div>
+        <div className="basis-3/4">
+          <p>Writing column</p>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/*.{tsx,ts,js}", "./src/**/*.{tsx,ts,js}"],
+  content: [
+    "./src/*.{tsx,js,ts,jsx,mdx}",
+    "./src/**/*.{tsx,js,ts,jsx,mdx}"
+  ],
   theme: {
     extend: {},
   },
