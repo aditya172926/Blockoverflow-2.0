61cd7392d2bff256ce6cb845d49dda61e5e4ebbd
Aditya <aditya26sg@gmail.com>
2024-07-01 03:29:33 +0530

    alert before reload

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,13 @@ import FileManager from "./components/FileManager";
 
 function App() {
 
+  window.addEventListener("beforeunload", (e: any) => {
+    e.preventDefault();
+    console.log(e)
+  });
+
   return (
-    <div className="container">
+    <div className="container" id="container">
       <div className="flex flex-row min-h-screen max-h-screen">
         <FileManager />
         <ContentArea />
